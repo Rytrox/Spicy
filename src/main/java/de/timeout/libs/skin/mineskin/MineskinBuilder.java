c880b07a2c4dc4f62af5fdfc8d50004c5b4e0c85
Timeout <taubmann870@gmail.com>
2021-07-09 22:56:53 +0200

    Finish MineskinBuilder. Not tested yet

--- src/main/java/de/timeout/libs/skin/mineskin/MineskinBuilder.java
+++ src/main/java/de/timeout/libs/skin/mineskin/MineskinBuilder.java
@@ -7,22 +7,68 @@ import java.io.File;
 import java.util.UUID;
 import java.util.concurrent.Future;
 
+/**
+ * Builder-Class that creates new Skins in Mineskin. <br>
+ * DONT USE THIS CLASS TOO OFTEN!! <br>
+ * <br>
+ * Best practise is creating one skin once and save his uuid in a file or database and grab the skin with
+ * {@link MineskinHandler#}
+ *
+ * @author Timeout
+ */
 public class MineskinBuilder {
 
     private final GenerateOptions options = new GenerateOptions();
 
+    /**
+     * Loads a Mineskin by its UUID. <br>
+     * <br>
+     * THIS IS NOT A REAL PLAYER's UUID, JUST A FAKE PLAYER's!!
+     *
+     * @param uuid the uuid of the mineskin
+     * @return a Future containing the mineskin
+     */
+    public static Future<Mineskin> loadMineskin(@NotNull UUID uuid) {
+        return MineskinHandler.getMineskin(uuid);
+    }
+
+    /**
+     * Sets the variant of the Mineskin. <br>
+     * The variant is the Model of the Skin (Steve, Alex)
+     *
+     * @param variant the variant you want to have. Standard value: Steve
+     * @return the builder to continue
+     */
     public MineskinBuilder variant(@NotNull Mineskin.Variant variant) {
         this.options.setVariant(variant.getType());
 
         return this;
     }
 
+    /**
+     * Sets the visibility of the skin. <br>
+     * <br>
+     * Skins with visibility 'private' will not be shown at mineskin.org gallery. <br>
+     * Skins with visibility 'public' will be shown at mineskin.org gallery. <br>
+     * <br>
+     * Both types will be saved internally by Mineskin.
+     *
+     * @param visibility the visibility you want to use. Standard value: private
+     * @return the builder to continue
+     */
     public MineskinBuilder visibility(@NotNull Mineskin.Visibility visibility) {
         this.options.setVisibility(visibility.getType());
 
         return this;
     }
 
+    /**
+     * Sets the name of the skin. <br>
+     * This has no effect to the skin. It's just a nice way to name your skins
+     *
+     * @param name the name you want to use. Standard value: ''
+     * @return the builder to continue
+     */
     public MineskinBuilder name(@NotNull String name) {
         Validate.isTrue(name.length() <= 20, "Name cannot be longer than 20 characters");
         this.options.setName(name);
@@ -30,15 +76,36 @@ public class MineskinBuilder {
         return this;
     }
 
+    /**
+     * Creates a new Mineskin. <br>
+     * This method returns a Future in a new Task, you need to wait or react to it with chainers
+     *
+     * @param url the url of the skin in the internet
+     * @return A Future containing the Mineskin
+     */
     public Future<Mineskin> generate(@NotNull String url) {
         return new MineskinHandler().generate(options, url);
     }
 
+    /**
+     * Creates a new Mineskin. <br>
+     * The method returns a Future in a new Task, you need to wait or react to it with chainers
+     *
+     * @param file the png file of the Skin
+     * @return A Future containing the Mineskin
+     */
     public Future<Mineskin> generate(@NotNull File file) {
-
+        return new MineskinHandler().generate(options, file);
     }
 
+    /**
+     * Creates a new Mineskin. <br>
+     * This method returns a Future in a new Task, you need to wait or react to it with chainers
+     *
+     * @param uuid the uuid of the player you want to grab the skin from
+     * @return A future containing the Mineskin
+     */
     public Future<Mineskin> generate(@NotNull UUID uuid) {
-
+        return new MineskinHandler().generate(options, uuid);
     }
 }
--- src/main/java/de/timeout/libs/skin/mineskin/MineskinHandler.java
+++ src/main/java/de/timeout/libs/skin/mineskin/MineskinHandler.java
@@ -6,52 +6,55 @@ import com.google.common.cache.LoadingCache;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import org.bukkit.craftbukkit.libs.org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
-public class MineskinHandler {
+class MineskinHandler {
 
     public static long REQUEST_TIMEOUT = 0;
 
     private static final ScheduledExecutorService scheduler = new ScheduledThreadPoolExecutor(0);
 
-    private static final LoadingCache<Integer, Mineskin> mineskinCache = CacheBuilder.newBuilder()
+    private static final LoadingCache<UUID, Mineskin> mineskinCache = CacheBuilder.newBuilder()
             .maximumSize(100L)
             .build(new CacheLoader<>() {
 
                 @Override
-                public Mineskin load(@NotNull Integer id) throws Exception {
+                public Mineskin load(@NotNull UUID id) throws Exception {
                     // Load from Mineskin-API:
-                    HttpClient client = HttpClient.newHttpClient();
                     HttpRequest request = HttpRequest.newBuilder()
                             .GET()
-                            .uri(URI.create("https://api.mineskin.org/get/id/" + id))
+                            .uri(URI.create("https://api.mineskin.org/get/uuid/" + id))
                             .build();
                     // Wait until next try
                     Thread.sleep(Math.max(System.currentTimeMillis() - REQUEST_TIMEOUT, 0));
 
                     // send Request
-                    JsonObject response = new JsonParser()
-                            .parse(client.send(request, HttpResponse.BodyHandlers.ofString()).body())
-                            .getAsJsonObject();
-                    REQUEST_TIMEOUT = System.currentTimeMillis() + response.get("nextRequest").getAsLong();
-
-                    // Convert to MineskinSkin
-                    return new Mineskin(response);
+                    return parseResponse(request);
                 }
 
             });
 
-    public MineskinHandler() {
-
+    public static Future<Mineskin> getMineskin(@NotNull UUID uuid) {
+        return scheduler.submit(() -> mineskinCache.get(uuid));
     }
 
     public Future<Mineskin> generate(@NotNull GenerateOptions options, @NotNull String url) {
@@ -60,35 +63,87 @@ public class MineskinHandler {
             JsonObject reqBody = new Gson().toJsonTree(options).getAsJsonObject();
             reqBody.addProperty("url", url);
 
-            // send request
-            HttpClient client = HttpClient.newHttpClient();
-
             HttpRequest request = HttpRequest.newBuilder(URI.create("https://api.mineskin.org/generate/url"))
                     .POST(HttpRequest.BodyPublishers.ofString(reqBody.toString()))
+                    .header("Content-Type", "application/json")
                     .build();
+
+            return parseResponse(request);
+        }, Math.max(System.currentTimeMillis() - REQUEST_TIMEOUT, 0), TimeUnit.MILLISECONDS);
+    }
+
+    public Future<Mineskin> generate(@NotNull GenerateOptions options, @NotNull File file) {
+        return scheduler.schedule(() -> {
+            // Build body
             try {
-                return parseResponse(client.send(request, HttpResponse.BodyHandlers.ofString()));
+                Map<String, String> body = new HashMap<>();
+                body.put("variant", options.getVariant());
+                body.put("name", options.getName());
+                body.put("visibility", String.valueOf(options.getVisibility()));
+                body.put("file", readFile(file));
+
+                String form = body.keySet().stream()
+                        .map(key -> key + "=" + URLEncoder.encode(body.get(key), StandardCharsets.UTF_8))
+                        .collect(Collectors.joining("&"));
+
+                HttpRequest request = HttpRequest.newBuilder(URI.create("https://api.mineskin.org/generate/upload"))
+                        .POST(HttpRequest.BodyPublishers.ofString(form))
+                        .build();
+
+                return parseResponse(request);
             } catch (IOException e) {
-                Logger.getGlobal().log(Level.SEVERE, "Unable to fetch Skin from Mineskin-API", e);
-            } catch (InterruptedException e) {
-                Logger.getGlobal().log(Level.WARNING, "INTERRUPTED: END THREAD. FATAL ERROR");
-                Thread.currentThread().interrupt();
+                Logger.getGlobal().log(Level.SEVERE, "Unable to read File", e);
             }
+
             return null;
         }, Math.max(System.currentTimeMillis() - REQUEST_TIMEOUT, 0), TimeUnit.MILLISECONDS);
     }
 
-    private Mineskin parseResponse(HttpResponse<String> response) throws IOException {
-        if(response.statusCode() == 200) {
-            JsonObject object = new JsonParser().parse(response.body()).getAsJsonObject();
-            Mineskin mineskin = new Mineskin(object);
+    public Future<Mineskin> generate(@NotNull GenerateOptions options, @NotNull UUID uuid) {
+        return scheduler.schedule(() -> {
+            // Build body
+            JsonObject reqBody = new Gson().toJsonTree(options).getAsJsonObject();
+            reqBody.addProperty("user", uuid.toString());
 
-            // cache
-            mineskinCache.put(mineskin.getID(), mineskin);
-            // update timeout
-            REQUEST_TIMEOUT = System.currentTimeMillis() + object.get("nextRequest").getAsLong();
+            HttpRequest request = HttpRequest.newBuilder(URI.create("https://api.mineskin.org/generate/user"))
+                    .POST(HttpRequest.BodyPublishers.ofString(reqBody.toString()))
+                    .header("Content-Type", "application/json")
+                    .build();
 
-            return mineskin;
-        } else throw new IOException("Error from Mineskin-API");
+            return parseResponse(request);
+        }, Math.max(System.currentTimeMillis() - REQUEST_TIMEOUT, 0), TimeUnit.MILLISECONDS);
+    }
+
+    private static Mineskin parseResponse(HttpRequest request) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() == 200) {
+                JsonObject object = new JsonParser().parse(response.body()).getAsJsonObject();
+                Mineskin mineskin = new Mineskin(object);
+
+                // cache
+                mineskinCache.put(mineskin.getUniqueID(), mineskin);
+                // update timeout
+                REQUEST_TIMEOUT = System.currentTimeMillis() + object.get("nextRequest").getAsLong();
+
+                return mineskin;
+            } else Logger.getGlobal().log(Level.WARNING, "Unhandled exception of Request. Code: {0}", response.statusCode());
+        } catch (IOException e) {
+            Logger.getGlobal().log(Level.SEVERE, "Unable to fetch Skin from Mineskin-API", e);
+        } catch (InterruptedException e) {
+            Logger.getGlobal().log(Level.WARNING, "INTERRUPTED: END THREAD. FATAL ERROR");
+            Thread.currentThread().interrupt();
+        }
+
+        return null;
+    }
+
+    private String readFile(File file) throws IOException {
+        // File to byte-array
+        byte[] data = FileUtils.readFileToByteArray(file);
+
+        return new BigInteger(data).toString();
     }
 }
