9198851c05357704dd8ea2f7033417c570fc8460
Timeout <taubmann870@gmail.com>
2021-12-20 12:47:06 +0100

    Add JavaDoc first version

--- src/main/java/de/timeout/libs/sql/SQL.java
+++ src/main/java/de/timeout/libs/sql/SQL.java
@@ -12,7 +12,23 @@ import java.sql.SQLException;
 public interface SQL {
 
     /**
-     * Prepares the MySQL-Connection for a new Statement
+     * Prepares the MySQL-Connection for a new Statement. <br>
+     * This Method will return a {@link QueryBuilder} so you need to decide which MySQL-Command you want to use. <br>
+     * <br>
+     * Normally you need to do something like that: <br>
+     * <br>
+     * <pre>
+     *     <code>
+     * mysql.prepare("SELECT * FROM ExampleTable WHERE id = ?", this.id)
+     *                 .query(ExampleEntity.class)
+     *                 .subscribe(List<ExampleEntity> entities -> {
+     *                      // Do something with your entities here!
+     *                 });
+     *     </code>
+     * </pre>
+     * <br>
+     * This method is fully asynchronous! If you want to use the Bukkit-API you may need a scheduler to sync this thread.
+     *
      * @param statement the raw statement
      * @param args any arguments if required
      * @return a builder which will be used for connections later
