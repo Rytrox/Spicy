09eea1271807b9d39746dc7363c8b260f519f5d3
Timeout <taubmann870@gmail.com>
2021-12-20 20:25:37 +0100

    Update to Java 17 and Minecraft 1.18.1

--- src/test/java/de/timeout/libs/item/ItemStacksTest.java
+++ src/test/java/de/timeout/libs/item/ItemStacksTest.java
@@ -1,12 +1,11 @@
 package de.timeout.libs.item;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
-import com.google.gson.Gson;
+
 import com.google.gson.JsonObject;
-import net.minecraft.locale.LocaleLanguage;
-import net.minecraft.nbt.NBTTagCompound;
+
+import de.timeout.libs.reflect.Reflections;
 import org.apache.commons.lang.WordUtils;
-import org.apache.commons.lang.reflect.FieldUtils;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_18_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
@@ -17,39 +16,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
 
 import static org.junit.Assert.*;
 
-@RunWith(PowerMockRunner.class)
-@SuppressStaticInitializationFor({ "de.timeout.libs.item.ItemStacks" })
-@PrepareForTest({ ItemStacks.class, ItemStack.class })
-@PowerMockIgnore({ "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "javax.management.*", "org.w3c.dom.*" })
+@RunWith(MockitoJUnitRunner.class)
 public class ItemStacksTest {
 
     private final ItemStack itemStack = new ItemStack(Material.STONE);
 
-    private final Class<?> itemstackClass = net.minecraft.world.item.ItemStack.class;
-    private final Class<?> craftItemStackClass = CraftItemStack.class;
-    private final Class<?> nbttagcompoundClass = NBTTagCompound.class;
-    private final Class<?> localelanguageClass = LocaleLanguage.class;
-
     @Before
     public void setup() throws IllegalAccessException {
-        PowerMockito.mockStatic(ItemStack.class);
         MockBukkit.mock();
-
-        FieldUtils.writeDeclaredStaticField(ItemStacks.class, "GSON", new Gson(), true);
-        FieldUtils.writeDeclaredStaticField(ItemStacks.class, "itemstackClass", itemstackClass, true);
-        FieldUtils.writeDeclaredStaticField(ItemStacks.class, "craftitemstackClass", craftItemStackClass, true);
-        FieldUtils.writeDeclaredStaticField(ItemStacks.class, "nbttagcompoundClass", nbttagcompoundClass, true);
-        FieldUtils.writeDeclaredStaticField(ItemStacks.class, "localelanguageClass", localelanguageClass, true);
     }
 
     @After
@@ -59,8 +39,6 @@ public class ItemStacksTest {
 
     @Test
     public void shouldEncodeItemStackToJSON() throws IllegalAccessException {
-        FieldUtils.writeDeclaredStaticField(ItemStacks.class, "GSON", new Gson(), true);
-
         JsonObject element = ItemStacks.encodeJson(itemStack);
 
         assertNotNull(element);
@@ -70,14 +48,15 @@ public class ItemStacksTest {
     @Test
     public void shouldDecodeJSONToItemStack() throws IllegalAccessException {
         // Tear up mock-methods (MockBukkit doesn't support ItemStack.deserialize yet...)
-        Mockito.when(ItemStack.deserialize(Mockito.any()))
-                .thenAnswer((inheritance) -> new ItemStack(itemStack));
-        FieldUtils.writeDeclaredStaticField(ItemStacks.class, "GSON", new Gson(), true);
+        try (MockedStatic<ItemStack> mockedStatic = Mockito.mockStatic(ItemStack.class, Mockito.CALLS_REAL_METHODS)) {
+            mockedStatic.when(() -> ItemStack.deserialize(Mockito.any()))
+                    .thenAnswer((inheritance) -> new ItemStack(itemStack));
 
-        JsonObject element = ItemStacks.encodeJson(itemStack);
-        ItemStack copy = ItemStacks.decodeJson(element);
+            JsonObject element = ItemStacks.encodeJson(itemStack);
+            ItemStack copy = ItemStacks.decodeJson(element);
 
-        assertEquals(this.itemStack, copy);
+            assertEquals(this.itemStack, copy);
+        }
     }
 
     @Test
@@ -114,9 +93,13 @@ public class ItemStacksTest {
 
     @Test
     public void shouldGetCustomNameFromLocalLanguage() {
-        try(MockedStatic<WordUtils> mockedStatic = Mockito.mockStatic(WordUtils.class)) {
+        try (MockedStatic<WordUtils> mockedStatic = Mockito.mockStatic(WordUtils.class);
+             MockedStatic<Reflections> reflectionsMockedStatic = Mockito.mockStatic(Reflections.class)) {
             mockedStatic.when(() -> WordUtils.capitalize(Mockito.anyString()))
                     .thenAnswer((invocationOnMock) -> "Test");
+            reflectionsMockedStatic.when(() -> Reflections.getCraftBukkitClass(Mockito.eq("inventory.CraftItemStack")))
+                    .thenAnswer(invocationOnMock -> CraftItemStack.class);
+
 
             String name = ItemStacks.getCustomizedName(itemStack);
             assertEquals("Test", name);
